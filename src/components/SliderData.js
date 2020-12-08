a82d07d677da66f461b5627593bea24359e1175f
AdroitMachine <anujsaxenamail@gmail.com>
2020-12-08 20:55:12 +0530

    initial commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,21 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1607349658516-9fb46b47ce53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'
+
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1607212136428-478c4db184a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1700&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1607212053115-d85fa8fddde4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1607140110438-7a17d182deee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1607344965879-a0fb7485f10a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=716&q=80'
+    }
+]
